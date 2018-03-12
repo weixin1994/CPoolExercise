445dd6c00d4c4b62f3ef8424972beb7784c6a8d2
WeiXin <690521211@qq.com>
2018-03-12 20:10:37 +0800

    first commit

---
+++ CPool_Day03/my_print_comb.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+int my_print_comb(void)
+{
+	int n = 0;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char div = ',';
+	while(n <= 999)
+	{
+		i = n / 100;
+		j = (n - i * 100) / 10;
+		k = n % 10;
+		if(i != j && j != k && k > j && j > i)
+		{
+			if(n > 1)
+				write(1,&div,1);
+			char a = i + '0';
+			char b = j + '0';
+			char c = k + '0';
+			write(1,&a,1);
+			write(1,&b,1);
+			write(1,&c,1);
+			
+		}
+		n++;
+	}
+	return 0;
+}
+int main()
+{
+	my_print_comb();
+	return  0;
+}
