e5b025200f6ee74869fa8fe0c7891fde8f1eb50a
WeiXin <690521211@qq.com>
2018-03-15 14:51:59 +0800

    achived by loop

--- CPool_Day05/my_compute_square_root.c
+++ CPool_Day05/my_compute_square_root.c
@@ -2,9 +2,21 @@
 int my_compute_square_root(int nb)
 {
 	int res = 0;
-	int n = nb;
-	for(int i = 0;i < 100;i++)
+	for(int i = 0;i <= nb;i++)
 	{
-		
+		if(i * i == nb)
+		{
+			res = i;
+			break;
+		}
+			
 	}
+	return res;
+}
+
+int main(void)
+{
+	printf("%d",my_compute_square_root(5));
+	printf("%d",my_compute_square_root(9));
+	return 0;
 }
