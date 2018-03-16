eb6d6861f58d40a1d3eb82c622e07cea5f1a2243
WeiXin <690521211@qq.com>
2018-03-16 15:48:53 +0800

    fomula error edition

--- firtree/tree.c
+++ firtree/tree.c
@@ -26,19 +26,14 @@ void my_print_echelon(int n,int max_space)
 }
 void tree(int size)
 {
-	int i;
-	int j;
-	int k;
+	int i,j,k,top,bot,space_count,pipe_size;
 	int ini = 4 ;
 	char star = '*';
 	char space = ' ';
-	int top;
-	int bot;
-	int space_count;
+	char pipe = '|';
 	int n = 1;
 	top = n * n + n - 1;
 	bot = size * size + size * 3 + 3;
-	
 	space_count = bot/2;
 	for(i = 1;i <= ini;i++)
 	{
@@ -51,10 +46,22 @@ void tree(int size)
 	}
 	for(int i = 2;i <= size;i++)
 		my_print_echelon(i,bot/2);
-	printf("%d",size);
+	
+	pipe_size = size;
+	if(size % 2 == 0)
+		pipe_size = size + 1;
+	for(int i = 0;i < size;i++)
+	{
+		for(int i = 0;i < bot/2-size/2;i++)
+			my_putchar(space);
+		for(int j = 0;j < pipe_size;j++)
+			my_putchar(pipe);
+		my_putchar('\n');
+	}
+	
 }	
 int main(void)
 {
-	tree(4);
+	tree(6);
 	return 0;
 }
