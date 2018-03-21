94115fa165138efa4c866af1bb3fb7b867db168b
WeiXin <690521211@qq.com>
2018-03-21 12:01:30 +0800

    fixed bugs

--- CPool_Day07mylibfunc/my_compute_power_rec.c
+++ CPool_Day07mylibfunc/my_compute_power_rec.c
@@ -2,11 +2,11 @@
 int my_compute_power_rec(int nb, int power)
 {
 	int res = 1;
-	if(p == 0)
+	if(power == 0)
 		res = 1;
-	if(p < 0)
+	if(power < 0)
 		res = 0;
-	if(p > 0)
+	if(power > 0)
 		res = nb * my_compute_power_rec(nb,power-1);
 	return res;
 }
