67e003d1b5a165fff71c193e9c50e3d367f5ddeb
WeiXin <690521211@qq.com>
2018-03-21 17:14:16 +0800

    perfect

--- CPool_Day07/task06/my_sort_params.c
+++ CPool_Day07/task06/my_sort_params.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 int my_putstr(char const *str);
-int my_strlen(char const *str);
 void my_putchar(char c);
-char *my_strcpy(char *dest, char const *src);
 int my_strcmp(char const *s1, char const *s2);
-
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	char temp[100];
-	for(int i = 0;i < argc-1;i++)
+	int temp;
+	int *order = malloc(sizeof(int) * argc);
+	for(int i = 0;i <= argc - 1;i++)
+		order[i] = i;
+	for(int i = 0;i < argc - 1;i++)
 	{
 		for(int j = 0;j < argc-1-i;j++)
 		{
-			if(my_strcmp(argv[j],argv[j+1]) > 0)
+			if(my_strcmp(argv[order[j]],argv[order[j+1]]) > 0)
 			{
-				my_strcpy(temp,argv[j+1]);
-				my_strcpy(argv[j+1],argv[j]);
-				my_strcpy(argv[j],temp);
-				
-				
-			/*	temp = tab[j+1];
-				tab[j+1] = tab[j];
-				tab[j] = temp;*/
+				temp = order[j+1];
+				order[j+1] = order[j];
+				order[j] = temp;	
 			}
 		}
 	}
-	for(int i = argc - 1; i >= 0; i--)
+	for(int i = 0; i <= argc - 1;i++)
 	{
-		my_putstr(argv[i]);
+		my_putstr(argv[order[i]]);
 		my_putchar('\n');
 	}
 	return 0;
